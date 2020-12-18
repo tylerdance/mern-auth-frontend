ef4dec5c7db62bdef2fd96b8eaf6138ec763a54e
Tyler Dance <tylerdance@Tylers-iMac.fios-router.home>
2020-12-18 11:56:01 -0500

    Add function and skeleton for components

---
+++ src/utils/setAuthToken.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+// this utility will add the authorized user's JWT to the request header
+// any routes that are protected will require the JWT in order to access them
+
+const setAuthToken = (token) => {
+    if (token) {
+        // Apply the token to every request header
+        axios.defaults.headers.common['Authorization'] = token;
+        console.log('--- Headers ---');
+        console.log(axios.defaults.headers.common);
+    }
+}
